a071e2cf3b7af14014d5c99b86a1217d26b501e4
Ivan <sanchezivan1919@gmail.com>
2026-02-15 13:43:06 +0100

    Añadido los cambios para mostrar un archivo HTML en nuestro proyecto de Spring Boot,también incluido un motor de plantillas y añadida su dependencia correspondiente

--- src/main/java/pildoras/proyecto1/Proyecto1/rest/MiRestController.java
+++ src/main/java/pildoras/proyecto1/Proyecto1/rest/MiRestController.java
@@ -1,16 +1,17 @@
 package pildoras.proyecto1.Proyecto1.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 
 public class MiRestController {
 
     @GetMapping("/")
     public String saludo (){
 
-            return  "Hola Mundo";
+            return  "index";
     }
 
 
