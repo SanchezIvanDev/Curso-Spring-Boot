b4cc68e4a765631df3a311e1a17cf688638c92fa
Ivan <sanchezivan1919@gmail.com>
2026-02-17 22:14:19 +0100

    añadida y configurada propiedades personalizadas

--- src/main/java/pildoras/proyecto1/Proyecto1/rest/MiRestController.java
+++ src/main/java/pildoras/proyecto1/Proyecto1/rest/MiRestController.java
@@ -1,5 +1,6 @@
 package pildoras.proyecto1.Proyecto1.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class MiRestController {
+
+    @Value("${alumno.pildoras.nombre}")
+    private String nombreAlumno;
+
+    @Value("${alumno.pildoras.apellido}")
+    private String apellidoAlumno;
+
+
+    @GetMapping("/Datos")
+    public String getDatos(){
+
+        return "Nombre del alumno: " + nombreAlumno + "<br>" + "Apellido del alumno: " + apellidoAlumno;
+
+    }
+
 
     @GetMapping("/")
     public String saludo (){
